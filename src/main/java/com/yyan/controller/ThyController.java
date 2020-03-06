bbb5583f4c9d302be857d6bab6e6c593e5e04afd
ranyanchuan <ranyanchuan@yimeijian.cn>
2020-03-06 17:59:04 +0800

    thy

---
+++ src/main/java/com/yyan/controller/ThyController.java
@@ -0,0 +1,47 @@
+package com.yyan.controller;
+
+import com.yyan.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
+/**
+ * springboot 整合 thymeleaf
+ */
+
+@Controller
+public class ThyController {
+
+    @RequestMapping("/showUser/thy")
+    public String showUser(HttpServletRequest request, Model model) {
+
+        //  集合
+        List<User> list = new ArrayList<>();
+        list.add(new User("xiaoming", 1, 22));
+        list.add(new User("xiaoming1", 2, 12));
+        list.add(new User("xiaoming2", 3, 32));
+
+        //  map
+        Map<String, User> map = new HashMap<>();
+        map.put("u1", new User("xiaoming", 1, 12));
+        map.put("u2", new User("xiaoming", 2, 22));
+        map.put("u3", new User("xiaoming", 3, 32));
+
+
+        model.addAttribute("msg", "springboot 整合 thymeleaf");
+        model.addAttribute("date", new Date());
+        model.addAttribute("gender", "男");
+        model.addAttribute("list", list);
+        model.addAttribute("map", map);
+
+        request.setAttribute("req", "HttpServletRequest");
+        //  没有验证成功¬
+        //  request.getSession().setAttribute("sess", "HttpSession");
+        //  request.getSession().getServletContext().setAttribute("app", "Application");
+
+        return "userListThy";
+    }
+}
