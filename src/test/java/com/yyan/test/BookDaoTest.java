5ea2f72976f9d2e1cee0da08ae56f27234f813a9
ranyanchuan <ranyanchuan@yimeijian.cn>
2020-03-08 15:35:00 +0800

    jpa query update

--- src/test/java/com/yyan/test/BookDaoTest.java
+++ src/test/java/com/yyan/test/BookDaoTest.java
@@ -3,6 +3,7 @@ package com.yyan.test;
 import com.yyan.App;
 import com.yyan.dao.BookDao;
 import com.yyan.dao.BookJpaRepository;
+import com.yyan.dao.BookJpaRepositoryQuery;
 import com.yyan.pojo.Book;
 import com.yyan.serviceImpl.UserServiceImpl;
 import org.junit.Test;
@@ -10,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,6 +24,8 @@ public class BookDaoTest {
     private BookDao bookDao;
     @Autowired
     private BookJpaRepository bookJpaRepository;
+    @Autowired
+    private BookJpaRepositoryQuery bookJpaRepositoryQuery;
 
     @Test
     public void testAddBook() {
@@ -35,18 +39,37 @@ public class BookDaoTest {
     public void testFindByTitle() {
 
         List<Book> list = bookJpaRepository.findByTitle("java");
-        System.out.println("list "+list.toString());
+        System.out.println("list " + list.toString());
     }
 
 
     @Test
     public void testFindByTitleAndNum() {
-        List<Book> list = bookJpaRepository.findByTitleAndNum("java",10);
-        System.out.println("list and "+list.toString());
+        List<Book> list = bookJpaRepository.findByTitleAndNum("java", 10);
+        System.out.println("list and " + list.toString());
     }
+
     @Test
     public void testFindByTitleOrNum() {
-        List<Book> list = bookJpaRepository.findByTitleOrNum("java",11);
-        System.out.println("list or "+list.toString());
+        List<Book> list = bookJpaRepository.findByTitleOrNum("java", 11);
+        System.out.println("list or " + list.toString());
+    }
+
+    @Test
+    public void testQueryByTitleUseHQL() {
+        List<Book> list = bookJpaRepositoryQuery.queryByTitleUseHQL("java");
+        System.out.println("list query " + list.toString());
+    }
+
+
+    /**
+     * 老版本 @Test 与  @Transactional 在一起会自动回滚
+     * @Rollback(false)  取消回滚
+     */
+
+    @Test
+    @Transactional // 加上事务
+    public void testUpdateByTitleUseHQL() {
+        bookJpaRepositoryQuery.updateByTitleUseHQL("javas", 1);
     }
 }
