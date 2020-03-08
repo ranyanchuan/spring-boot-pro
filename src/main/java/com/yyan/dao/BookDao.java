ab6a7f4d3b875f00e415495c5c3d93cc58b4c5de
ranyanchuan <ranyanchuan@yimeijian.cn>
2020-03-08 09:59:28 +0800

    jpa Repository

--- src/main/java/com/yyan/dao/BookDao.java
+++ src/main/java/com/yyan/dao/BookDao.java
@@ -6,8 +6,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * T: 当前需要映射的实体
  * TD: 当前映射的实体中 OID 的类型
+ * ========核心接口=====
+ * 1、Repository  接口
+ *    提供了方法名称命名查询方式
+ *    提供了基于 @query 注解查询与更新
+ * 2、CrudRepository  接口
+ * 3、PageAndSortingRepository  接口
+ * 4、JpaRepository  接口
+ * 5、JpaSpecificationExecutor  接口
+ *
  */
+
+
 
 public interface BookDao extends JpaRepository<Book, Integer> {
 
 }
+
+
--- src/test/java/com/yyan/test/BookDaoTest.java
+++ src/test/java/com/yyan/test/BookDaoTest.java
@@ -2,6 +2,7 @@ package com.yyan.test;
 
 import com.yyan.App;
 import com.yyan.dao.BookDao;
+import com.yyan.dao.BookJpaRepository;
 import com.yyan.pojo.Book;
 import com.yyan.serviceImpl.UserServiceImpl;
 import org.junit.Test;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {App.class})
 
@@ -17,6 +20,8 @@ public class BookDaoTest {
 
     @Autowired
     private BookDao bookDao;
+    @Autowired
+    private BookJpaRepository bookJpaRepository;
 
     @Test
     public void testAddBook() {
@@ -24,5 +29,24 @@ public class BookDaoTest {
         book.setNum(10);
         book.setTitle("java");
         bookDao.save(book);
+    }
+
+    @Test
+    public void testFindByTitle() {
+
+        List<Book> list = bookJpaRepository.findByTitle("java");
+        System.out.println("list "+list.toString());
+    }
+
+
+    @Test
+    public void testFindByTitleAndNum() {
+        List<Book> list = bookJpaRepository.findByTitleAndNum("java",10);
+        System.out.println("list and "+list.toString());
+    }
+    @Test
+    public void testFindByTitleOrNum() {
+        List<Book> list = bookJpaRepository.findByTitleOrNum("java",11);
+        System.out.println("list or "+list.toString());
     }
 }
